f9ed1f94aff76e551d70fa9a4481f823951b3f79
Carol <carol@b25>
2025-03-18 12:38:46 +0100

    comienoz tema

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    private int id;
+    private String name;
+    private Set<String>tags;
+}
